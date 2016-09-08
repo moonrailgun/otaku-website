dc572586c2811e1af19dc562cb92bcc5ef099312
moonrailgun <moonrailgun@gmail.com>
2016-09-08 12:01:17 +0800

    创建应用 上传部分
    ADD 创建应用 上传部分

--- account/otaku/createApp.php
+++ account/otaku/createApp.php
@@ -9,17 +9,29 @@ if (Common::isPost()) {
         OSAdmin::alert("error", ErrorMessage::NEED_PARAM);
     } else {
         //保存文件到本地
+        if ($_FILES["file"]["type"] == "application/x-zip-compressed") {
+            if ($_FILES["file"]["error"] > 0) {
+                OSAdmin::alert("error", ErrorMessage::ERROR);
+            } else {
+                $file_extend = pathinfo($_FILES["file"]["name"])["extension"]; //文件扩展名
+                $guid        = Common::guid();
+                if (is_uploaded_file($_FILES['file']['tmp_name']) && move_uploaded_file($_FILES['file']['tmp_name'], __DIR__ . '../upload/' . $guid . "." . $file_extend)) {
+                    $file_path = ADMIN_URL . '/upload/' . $guid . "." . $file_extend;
 
-        $file_path;
+                    $input_data = array('app_name' => $app_name, 'app_type' => $app_type, 'app_version' => $app_version, 'app_author' => $app_author, 'app_icon' => $app_icon, 'app_description' => $app_description, 'app_screenshots' => $app_screenshots_json, 'app_file' => $file_path, 'owner_id' => $owner_id);
+                    $app_id     = App::createApp($input_data);
 
-        $input_data = array('app_name' => $app_name, 'app_type' => $app_type, 'app_version' => $app_version, 'app_author' => $app_author, 'app_icon' => $app_icon, 'app_description' => $app_description, 'app_screenshots' => $app_screenshots_json, 'app_file' => $file_path, 'owner_id' => $owner_id);
-        $user_id    = App::createApp($input_data);
-
-        if ($user_id) {
-            SysLog::addLog(UserSession::getUserName(), 'ADD', 'App', $user_id, json_encode($input_data));
-            Common::exitWithSuccess('账号添加成功', 'otaku/createApp.php');
+                    if ($app_id) {
+                        SysLog::addLog(UserSession::getUserName(), 'ADD', 'App', $user_id, json_encode($input_data));
+                        Common::exitWithSuccess('账号添加成功', 'otaku/createApp.php');
+                    } else {
+                        OSAdmin::alert("error");
+                    }
+                }
+            }
         } else {
-            OSAdmin::alert("error");
+            //类型错误
+            OSAdmin::alert("error", ErrorMessage::ERROR_FILE_TYPE);
         }
     }
 }
